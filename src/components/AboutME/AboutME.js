a6bd234aadf0cb6a25eb1fa90d5af00e21f61b33
Anjali Sharma <52730168+AnjaliSharma1234@users.noreply.github.com>
2021-04-14 18:31:28 +0530

    Updated About section

--- src/components/AboutME/AboutME.js
+++ src/components/AboutME/AboutME.js
@@ -8,18 +8,18 @@ export default class AboutME extends Component {
                         <div className="col-full text-center">
                             <h3>About Me</h3>
                             <h1>"A Renaissance Soul"</h1>
-                            <p className="lead">I am a final year undergraduate having a keen interest in exploring everything. This website compiles my diverse experiences and ideas.</p>
+                            <p className="lead">Hey there, welcome to my world of diverse experiences and ideas!</p>
                         </div>
-                        <div className="col-full text-center">
-                            <h3>Key Strengths</h3>
-                            <h4>Passion for Problem-Solving</h4>
-                            <p className="lead">I will be helpful in analyzing the bigger picture of the problem faced by the team by dividing it into smaller solvable modules, both qualitatively and quantitatively, backed up by facts and not only gut.</p>
-                            <h4>Getting Things Done Attitude</h4>
-                            <p className="lead">My one year of startup experience has taught me to wear many hats, deal with close deadlines, failure management, and to adapt change effectively according to the requirements to complete the task in hand.</p>
-                            <h4>Communication Skills</h4>
-                            <p className="lead">I have been seen as a public speaker and a team player facilitating the two-way communication process. This helps me to listen and present my ideas with clarity and right intent throughout the team.</p>
+//                         <div className="col-full text-center">
+// //                             <h3>Key Strengths</h3>
+// //                             <h4>Passion for Problem-Solving</h4>
+// //                             <p className="lead">I will be helpful in analyzing the bigger picture of the problem faced by the team by dividing it into smaller solvable modules, both qualitatively and quantitatively, backed up by facts and not only gut.</p>
+// //                             <h4>Getting Things Done Attitude</h4>
+// //                             <p className="lead">My one year of startup experience has taught me to wear many hats, deal with close deadlines, failure management, and to adapt change effectively according to the requirements to complete the task in hand.</p>
+// //                             <h4>Communication Skills</h4>
+// //                             <p className="lead">I have been seen as a public speaker and a team player facilitating the two-way communication process. This helps me to listen and present my ideas with clarity and right intent throughout the team.</p>
                         
-                        </div>
+//                         </div>
                     </div>
                 </section> {/* end about */}
                 
