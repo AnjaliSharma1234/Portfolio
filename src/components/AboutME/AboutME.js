d74dea5f1d1ed4ebe63a3b381089dea5da4073b9
Anjali Sharma <52730168+AnjaliSharma1234@users.noreply.github.com>
2021-07-21 11:43:01 +0530

    updated about

--- src/components/AboutME/AboutME.js
+++ src/components/AboutME/AboutME.js
@@ -7,7 +7,7 @@ export default class AboutME extends Component {
                     <div className="row narrow section-intro has-bottom-sep">
                         <div className="col-full text-center">
                             <h3>NEWSLETTER</h3>
-                            <h1>Great Question by Anjali Sharma</h1>
+                            <h1>Great Question</h1>
                             <p className="lead">Not news, definitely not a letter, just decoding careers, products, startups, and life by answering one great question at a time!</p>
                         </div>
                         <div>
